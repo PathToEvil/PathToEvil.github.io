a3bfb3d8171fec8744b812a91e239f879121118e
DeMoN <wanghao921228@163.com>
2018-03-02 18:27:47 +0800

    right

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
     $("#cartoon").attr("src", "image/cartoon.jpeg");
+    $("#contact1").attr("src", "image/github.png");
+    $("#contact2").attr("src", "image/jianshu.png");
+})
+
+$("#contact1").click(function () {
+    alert("github")
+})
+
+$("#contact2").click(function () {
+    alert("简书")
 })
