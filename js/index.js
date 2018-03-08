236acd4db42434bcbd435bf51499cb39d6028779
DeMoN <wanghao921228@163.com>
2018-03-08 18:25:31 +0800

    左侧点击

--- js/index.js
+++ js/index.js
@@ -1,13 +1,43 @@
 $(document).ready(function () {
+
+    configLeft();
+    configRight();
+
+
+})
+
+function configLeft() {
+    /*默认选中第一个*/
+    $("#left ul li")[0].className = "selected"
+    $("#left ul li").each(function (obj) {
+        $(this).click(function () {
+            if (this.className == "selected") {
+                return
+            }else {
+                $("#left ul li").each(function () {
+                    $(this).removeClass()
+                })
+                this.className = "selected"
+            }
+        })
+    })
+}
+
+/*配置右侧*/
+function configRight() {
+
     $("#cartoon").attr("src", "image/cartoon.jpeg");
-    $("#contact1").attr("src", "image/github.png");
-    $("#contact2").attr("src", "image/jianshu.png");
-})
 
-$("#contact1").click(function () {
-    alert("github")
-})
+    var github = $("#github");
+    github.attr("src", "image/github.png");
+    github.click(function () {
+        window.open("https://github.com/PathToEvil");
+    });
 
-$("#contact2").click(function () {
-    alert("简书")
-})
+    var jianshu = $("#jianshu");
+    jianshu.attr("src", "image/jianshu.png");
+    jianshu.click(function () {
+        alert("简书");
+    });
+}
+
